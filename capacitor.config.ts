6e97017b3104eb87b868d5325093188c7e834b3c
samu97rm <samuelramos@metricool.com>
2026-03-29 12:26:34 +0200

    refactor: remove Capacitor config and native platform directories

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.applec.app111',
-  appName: 'Applec',
-  webDir: 'build'
-};
-
-export default config;
