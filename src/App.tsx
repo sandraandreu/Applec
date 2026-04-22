551b5aa886cf0159c5e1bc1d4add73e83d0d68f8
sandraandreu <sandreum08@gmail.com>
2026-04-22 11:07:47 +0200

    fix(a11y): wrap app routes in main landmark element

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ const App: React.FC = () => (
   <AuthContextProvider>
     <GroupContextProvider>
       <div className="app">
-        <AppRoutes />
+        <main className="app__main">
+          <AppRoutes />
+        </main>
       </div>
     </GroupContextProvider>
   </AuthContextProvider>
