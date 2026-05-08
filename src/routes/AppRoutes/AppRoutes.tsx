64176997c9beea40a7b2d71d2337873a93f0b4b2
sandraandreu <sandreum08@gmail.com>
2026-05-08 08:53:27 +0200

    perf(routes): add lazy loading to all pages

--- src/routes/AppRoutes/AppRoutes.tsx
+++ src/routes/AppRoutes/AppRoutes.tsx
@@ -1,164 +1,168 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import PrivateRoutes from "./PrivateRoutes";
 import PublicRoutes from "./PublicRoutes";
 import MainLayout from "../../components/layout/MainLayout";
+import Loading from "../../components/loading/Loading";
 
-import Home from "../../pages/home/Home";
-import LoginPage from "../../pages/auth/login/LoginPage";
-import RegisterPage from "../../pages/auth/register/RegisterPage";
-import ForgotPasswordPage from "../../pages/auth/forgot-password/ForgotPasswordPage";
-import CreateGroupPage from "../../pages/groups/create-group/CreateGroupPage";
-import WelcomePage from "../../pages/onboarding/welcome/WelcomePage";
-import GroupPage from "../../pages/onboarding/group/GroupPage";
-import LanguagePage from "../../pages/onboarding/language/LanguagePage";
-import JoinGroupPage from "../../pages/groups/join-group/JoinGroupPage";
-import InviteGroupPage from "../../pages/groups/invite-group/InviteGroupPage";
-import LandingPage from "../../pages/onboarding/landing/LandingPage";
-import MembersPage from "../../pages/members/MembersPage";
-import EventsListPage from "../../pages/events/events-list/EventsListPage";
-import EventDetailPage from "../../pages/events/event-detail/EventDetailPage";
-import StyleGuide from "../../pages/style-guide/StyleGuide";
+const Home = lazy(() => import("../../pages/home/Home"));
+const LoginPage = lazy(() => import("../../pages/auth/login/LoginPage"));
+const RegisterPage = lazy(() => import("../../pages/auth/register/RegisterPage"));
+const ForgotPasswordPage = lazy(() => import("../../pages/auth/forgot-password/ForgotPasswordPage"));
+const CreateGroupPage = lazy(() => import("../../pages/groups/create-group/CreateGroupPage"));
+const WelcomePage = lazy(() => import("../../pages/onboarding/welcome/WelcomePage"));
+const GroupPage = lazy(() => import("../../pages/onboarding/group/GroupPage"));
+const LanguagePage = lazy(() => import("../../pages/onboarding/language/LanguagePage"));
+const JoinGroupPage = lazy(() => import("../../pages/groups/join-group/JoinGroupPage"));
+const InviteGroupPage = lazy(() => import("../../pages/groups/invite-group/InviteGroupPage"));
+const LandingPage = lazy(() => import("../../pages/onboarding/landing/LandingPage"));
+const MembersPage = lazy(() => import("../../pages/members/MembersPage"));
+const EventsListPage = lazy(() => import("../../pages/events/events-list/EventsListPage"));
+const EventDetailPage = lazy(() => import("../../pages/events/event-detail/EventDetailPage"));
+const StyleGuide = lazy(() => import("../../pages/style-guide/StyleGuide"));
 
 const AppRoutes = () => (
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navigate to="/landing" replace />} />
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/landing" replace />} />
 
-      <Route
-        path="/home"
-        element={
-          <PrivateRoutes requiresGroup>
-            <MainLayout>
-              <Home />
-            </MainLayout>
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoutes requiresGroup>
+              <MainLayout>
+                <Home />
+              </MainLayout>
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/landing"
-        element={
-          <PublicRoutes>
-            <LandingPage />
-          </PublicRoutes>
-        }
-      />
+        <Route
+          path="/landing"
+          element={
+            <PublicRoutes>
+              <LandingPage />
+            </PublicRoutes>
+          }
+        />
 
-      <Route
-        path="/register"
-        element={
-          <PublicRoutes>
-            <RegisterPage />
-          </PublicRoutes>
-        }
-      />
+        <Route
+          path="/register"
+          element={
+            <PublicRoutes>
+              <RegisterPage />
+            </PublicRoutes>
+          }
+        />
 
-      <Route
-        path="/login"
-        element={
-          <PublicRoutes>
-            <LoginPage />
-          </PublicRoutes>
-        }
-      />
+        <Route
+          path="/login"
+          element={
+            <PublicRoutes>
+              <LoginPage />
+            </PublicRoutes>
+          }
+        />
 
-      <Route
-        path="/forgot-password"
-        element={
-          <PublicRoutes>
-            <ForgotPasswordPage />
-          </PublicRoutes>
-        }
-      />
+        <Route
+          path="/forgot-password"
+          element={
+            <PublicRoutes>
+              <ForgotPasswordPage />
+            </PublicRoutes>
+          }
+        />
 
-      <Route
-        path="/onboarding/welcome"
-        element={
-          <PrivateRoutes>
-            <WelcomePage />
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/onboarding/welcome"
+          element={
+            <PrivateRoutes>
+              <WelcomePage />
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/onboarding/language"
-        element={
-          <PrivateRoutes>
-            <LanguagePage />
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/onboarding/language"
+          element={
+            <PrivateRoutes>
+              <LanguagePage />
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/onboarding/group"
-        element={
-          <PrivateRoutes>
-            <GroupPage />
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/onboarding/group"
+          element={
+            <PrivateRoutes>
+              <GroupPage />
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/create-group"
-        element={
-          <PrivateRoutes>
-            <CreateGroupPage />
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/create-group"
+          element={
+            <PrivateRoutes>
+              <CreateGroupPage />
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/invite-group"
-        element={
-          <PrivateRoutes>
-            <InviteGroupPage />
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/invite-group"
+          element={
+            <PrivateRoutes>
+              <InviteGroupPage />
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/join-group"
-        element={
-          <PrivateRoutes>
-            <JoinGroupPage />
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/join-group"
+          element={
+            <PrivateRoutes>
+              <JoinGroupPage />
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/members"
-        element={
-          <PrivateRoutes requiresGroup>
-            <MainLayout>
-              <MembersPage />
-            </MainLayout>
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/members"
+          element={
+            <PrivateRoutes requiresGroup>
+              <MainLayout>
+                <MembersPage />
+              </MainLayout>
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/events"
-        element={
-          <PrivateRoutes requiresGroup>
-            <MainLayout>
-              <EventsListPage />
-            </MainLayout>
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/events"
+          element={
+            <PrivateRoutes requiresGroup>
+              <MainLayout>
+                <EventsListPage />
+              </MainLayout>
+            </PrivateRoutes>
+          }
+        />
 
-      <Route
-        path="/events/:id"
-        element={
-          <PrivateRoutes requiresGroup>
-            <EventDetailPage />
-          </PrivateRoutes>
-        }
-      />
+        <Route
+          path="/events/:id"
+          element={
+            <PrivateRoutes requiresGroup>
+              <EventDetailPage />
+            </PrivateRoutes>
+          }
+        />
 
-      <Route path="/style-guide" element={<StyleGuide />} />
-    </Routes>
+        <Route path="/style-guide" element={<StyleGuide />} />
+      </Routes>
+    </Suspense>
   </BrowserRouter>
 );
 
