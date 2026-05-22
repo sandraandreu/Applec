f458c4a9ac34577169c1dfb7e40e08b573becb78
samu97rm <samuelramos@metricool.com>
2026-05-22 19:19:43 +0200

    new capacitor 8 with iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.applec.app',
+  appName: 'Applec',
+  webDir: 'build'
+};
+
+export default config;
