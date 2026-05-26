9312218e5245e019b21cb397a8ca2c89ba61f818
sandraandreu <sandreum08@gmail.com>
2026-05-26 21:05:21 +0200

    fix(language-settings): use surface background instead of gradient

--- src/pages/profile/language/LanguageSettingsPage.tsx
+++ src/pages/profile/language/LanguageSettingsPage.tsx
@@ -1,17 +1,12 @@
 import "./language-settings.scss";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { useAuthContext } from "../../../context/auth/AuthContext";
-import useLayoutBackground from "../../../hooks/useLayoutBackground";
 import PageHeader from "../../../components/layout/PageHeader";
 import LanguageSelector from "../../../components/language-selector/LanguageSelector";
 
 const LanguageSettingsPage = () => {
   const { t } = useTranslation("profile");
-  const { profile } = useAuthContext();
   const navigate = useNavigate();
-
-  useLayoutBackground(profile?.role);
 
   return (
     <div className="language-settings-page">
